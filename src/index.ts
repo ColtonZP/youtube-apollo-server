dd1ca6cca16cf7d57aac510dac29b0baa0c54512
Colton <czpemberton@gmail.com>
2021-05-24 10:28:44 -0700

    gcloud setup

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import { ApolloServer, ApolloError, ValidationError, gql } from 'apollo-server'
 import { schema } from './schema'
 import { dataSources } from './dataSource'
 
-const server = new ApolloServer({ schema, dataSources })
+const server = new ApolloServer({
+  schema,
+  dataSources,
+})
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
